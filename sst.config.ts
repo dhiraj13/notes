16876bd64932822aad27b76ed61f0e95f782e1b8
Dhiru <dkhapangi13@outlook.com>
2024-09-20 11:02:34 +0545

    add auth to our serverless app

--- sst.config.ts
+++ sst.config.ts
@@ -9,7 +9,15 @@ export default $config({
     };
   },
   async run() {
-    await import("./infra/storage");
     await import("./infra/api");
+    await import("./infra/storage");
+    const auth = await import("./infra/auth");
+
+    return {
+      UserPool: auth.userPool.id,
+      Region: aws.getRegionOutput().name,
+      IdentityPool: auth.identityPool.id,
+      UserPoolClient: auth.userPoolClient.id,
+    }
   },
 });
