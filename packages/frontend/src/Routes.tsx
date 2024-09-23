c3d070b36987e701092f2254058a930b3164e3f3
Dhiru <dkhapangi13@outlook.com>
2024-09-23 12:31:15 +0545

    display a note

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
+
 import Home from "./containers/Home.tsx";
 import Login from "./containers/Login.tsx";
+import Notes from "./containers/Notes.tsx";
 import Signup from "./containers/Signup.tsx";
 import NewNote from "./containers/NewNote.tsx";
 import NotFound from "./containers/NotFound.tsx";
@@ -13,6 +15,7 @@ export default function Links() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/notes/new" element={<NewNote />} />
       <Route path="*" element={<NotFound />} />;
+      <Route path="/notes/:id" element={<Notes />} />
     </Routes>
   );
 }
